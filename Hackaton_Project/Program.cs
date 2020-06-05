bcb0fcf83a70a77c740efac8bdbcf9096446fabf
Ilya Rud <ilya.rud1@mail.ru>
2020-06-05 22:40:45 +0300

    Ну он грузит файл

--- Hackaton_Project/Program.cs
+++ Hackaton_Project/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Hackaton_Project
+namespace ConsoleApp2
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "Resource\\bad.csv";
+            List<string> listAddres = new List<string>();
+            using (var reader = new StreamReader(path))
+            {
+                reader.ReadLine(); //костыль
+                while (!reader.EndOfStream)
+                {
+                    string str = reader.ReadLine();
+                    string[] strArr = str.Split(';');
+                    str = str.Remove(0, strArr[0].Count() + 1);
+                    listAddres.Add(str);
+                }
+
+            }
+
         }
     }
 }
